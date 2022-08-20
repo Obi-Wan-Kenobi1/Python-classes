aabd6277278f61aeeedbf006dec70f7e60a3edd8
Obi-Wan-Kenobi1 <benjamin.hamroune@gmail.com>
2022-08-20 14:53:55 -0400

    A basic calculator with functions

--- Basiccalculator1.py
+++ Basiccalculator1.py
@@ -1,34 +1,35 @@
-numbers = input("Enter all of your numbers")
-
+numbers = input("Enter all of your numbers.")
+numbers.split(" ")
+print(numbers)
 def addition1(numbers):
     for i in numbers:
         sum1 = 0
-        sum1 += i
+        sum1 += float(i)
     return sum1
 
 def subtraction1(numbers):
     for i in numbers:
         sub = 0
-        sub -= i
+        sub -= float(i)
     return sub
 
 def multiplication1(numbers):
     for i in numbers:
         multiple = 0
-        multiple *= i
+        multiple *= float(i)
     return multiple
 
 def division1(numbers):
     for i in numbers:
         divident = 0
-        divident /= i
+        divident /= float(i)
     return divident
 
 while True:
     quit1 = input("Do you want to quit?").lower()
     if quit1 == "yes":
         break
-    elif quit == "no":
+    elif quit1 == "no":
         operation = input("Which operation do you want to use? Multiplication, Division, Addition ot Subtracrion?").lower()
         if operation == "addition":
             print(addition1(numbers))
